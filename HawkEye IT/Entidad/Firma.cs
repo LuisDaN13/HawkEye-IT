2428ef250e024292d6065122810d0662b46fe45e
Luis Núñez Chacón <nunez9076@gmail.com>
2026-07-20 14:01:42 -0600

    Ultima Versión

---
+++ HawkEye IT/Entidad/Firma.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HawkEye_IT.Entidad
+{
+    internal class Firma
+    {
+        private const string ClaveSecreta = "FLDSMDFRfldsmdfr";
+
+        public static string FirmarPayload(string jsonPayload)
+        {
+            using var hmac = new HMACSHA512(Encoding.UTF8.GetBytes(ClaveSecreta));
+            byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(jsonPayload));
+            return Convert.ToHexString(hash); // ej: "A1B2C3D4..."
+        }
+    }
+}
